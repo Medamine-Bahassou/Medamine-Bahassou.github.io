57d4c1c064663072f20c88e7b5ae97340c76a140
Medamine-bahassou <mohamedamine.bahassou@gmail.com>
2024-06-14 16:06:53 +0100

    views-counter

--- java.js
+++ java.js
@@ -22,3 +22,26 @@ window.addEventListener('scroll', () => {
         document.getElementById('navbar').style.boxShadow = "none";
     }
 });
+
+
+// 
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = `<i class="far fa-eye"></i> ${visitCount}`;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = `<i class="far fa-eye"></i> ${visitCount}`;
+});
